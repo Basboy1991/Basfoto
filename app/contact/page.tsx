d108b127ac6d6e06ca1fdd981812f9f449f67e14
Basboy1991 <basvanderscheer@gmail.com>
2025-12-22 01:05:28 +0100

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
   return <h1>Hello Next.js!</h1>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZTKBMGXLDK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZTKBMGXLDK');
+</script>
 }
