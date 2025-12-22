e14fa1ba213d141075e30fd022ca3f8f02009c9d
Basboy1991 <basvanderscheer@gmail.com>
2025-12-22 19:00:19 +0100

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -3,6 +3,7 @@ import album from "./album";
 import homePage from "./homePage";
 import portfolioItem from "./portfolioItem";
 import packageType from "./package";
+import availabilitySettings from "./availabilitySettings";
 export const schema = {
-  types: [sitePage, album, homePage, packageType],
+  types: [sitePage, album, homePage, packageType, availabilitySettings],
 };
