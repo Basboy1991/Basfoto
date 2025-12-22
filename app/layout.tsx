e18132acf44e9f431e1d70e07df5e114ef72ae0f
Basboy1991 <basvanderscheer@gmail.com>
2025-12-22 13:18:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 import { siteConfig } from "@/config/site";
 import SiteHeader from "@/components/SiteHeader";
 import SiteFooter from "@/components/SiteFooter";
+
+const GA_ID = "G-ZTKBMGXLDK";
 
 export const metadata: Metadata = {
   title: {
@@ -24,6 +27,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           color: "var(--text)",
         }}
       >
+        {/* Google Analytics (globaal, 1x) */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
+
         {/* Wrapper houdt footer onderaan */}
         <div className="flex min-h-screen flex-col">
           <SiteHeader />
