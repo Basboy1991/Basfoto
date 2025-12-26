acf18a500e88a25073e034ef1eaaf289983adbc8
Basboy1991 <basvanderscheer@gmail.com>
2025-12-26 23:53:50 +0100

    Update env.server.ts

--- sanity/env.server.ts
+++ sanity/env.server.ts
@@ -2,8 +2,11 @@
 export const projectId = process.env.SANITY_PROJECT_ID || "";
 export const dataset = process.env.SANITY_DATASET || "";
 
-export const apiVersion =
-  process.env.SANITY_API_VERSION || "2024-01-01";
+// altijd geldige apiVersion
+export const apiVersion = process.env.SANITY_API_VERSION || "2024-01-01";
+
+export const writeToken = process.env.SANITY_API_WRITE_TOKEN || "";
 
 if (!projectId) throw new Error("Missing SANITY_PROJECT_ID");
 if (!dataset) throw new Error("Missing SANITY_DATASET");
+if (!writeToken) throw new Error("Missing SANITY_API_WRITE_TOKEN");
