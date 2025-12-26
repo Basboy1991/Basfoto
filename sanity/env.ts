47e6dc9e0ba8750aaf7e0490f0401ade510316d6
Basboy1991 <basvanderscheer@gmail.com>
2025-12-27 00:06:31 +0100

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,8 +1,15 @@
 // sanity/env.ts
-export const projectId = process.env.SANITY_PROJECT_ID!;
-export const dataset = process.env.SANITY_DATASET!;
-export const apiVersion =
-  process.env.SANITY_API_VERSION || "2024-01-01";
+export const projectId =
+  process.env.SANITY_PROJECT_ID ||
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  "";
 
-if (!projectId) throw new Error("Missing SANITY_PROJECT_ID");
-if (!dataset) throw new Error("Missing SANITY_DATASET");
+export const dataset =
+  process.env.SANITY_DATASET ||
+  process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  "";
+
+export const apiVersion =
+  process.env.SANITY_API_VERSION ||
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION ||
+  "2024-01-01";
