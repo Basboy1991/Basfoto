09540a1a8167d94eb6e97473a9856e9111fa428a
Basboy1991 <basvanderscheer@gmail.com>
2025-12-26 23:50:57 +0100

    Create env.server.ts

---
+++ sanity/env.server.ts
@@ -0,0 +1,9 @@
+// sanity/env.server.ts
+export const projectId = process.env.SANITY_PROJECT_ID || "";
+export const dataset = process.env.SANITY_DATASET || "";
+
+export const apiVersion =
+  process.env.SANITY_API_VERSION || "2024-01-01";
+
+if (!projectId) throw new Error("Missing SANITY_PROJECT_ID");
+if (!dataset) throw new Error("Missing SANITY_DATASET");
