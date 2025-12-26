3685201fd960ff88da5dceb2c77abc2592b7b967
Basboy1991 <basvanderscheer@gmail.com>
2025-12-26 22:48:37 +0100

    Update schemaTypes.ts

--- sanity/schemaTypes.ts
+++ sanity/schemaTypes.ts
@@ -1,5 +1,4 @@
 // sanity/schemaTypes.ts
-
 import sitePage from "./schemaTypes/sitePage";
 import album from "./schemaTypes/album";
 import homePage from "./schemaTypes/homePage";
@@ -7,12 +6,6 @@ import portfolioItem from "./schemaTypes/portfolioItem";
 import packageType from "./schemaTypes/package";
 import availabilitySettings from "./schemaTypes/availabilitySettings";
 import bookingRequest from "./schemaTypes/bookingRequest";
-
-// (optioneel) als je ze gebruikt:
-import blockContentType from "./schemaTypes/blockContentType";
-import postType from "./schemaTypes/postType";
-import authorType from "./schemaTypes/authorType";
-import categoryType from "./schemaTypes/categoryType";
 
 export const schemaTypes = [
   sitePage,
@@ -22,11 +15,6 @@ export const schemaTypes = [
   packageType,
   availabilitySettings,
   bookingRequest,
-
-  // optioneel (laat staan als ze in je studio gebruikt worden)
-  blockContentType,
-  postType,
-  authorType,
-  categoryType,
 ];
 
+export default schemaTypes;
