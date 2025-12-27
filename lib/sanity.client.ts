a1c3e309d7d588b76d6c470ff49c935771d67d6d
Basboy1991 <basvanderscheer@gmail.com>
2025-12-27 10:10:57 +0100

    Update sanity.client.ts

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,15 +1,19 @@
 import { createClient } from "next-sanity";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-01-01";
+
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
-  useCdn: true, // snel
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true, // snel voor content
 });
 
 export const sanityClientFresh = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
-  useCdn: false, // ✅ altijd vers
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false, // altijd up-to-date (voor boekingen)
 });
