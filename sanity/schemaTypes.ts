37bd0c40aec17fb95f1d9393767aecdf96d9e545
Basboy1991 <basvanderscheer@gmail.com>
2025-12-29 11:52:57 +0100

    Update schemaTypes.ts

--- sanity/schemaTypes.ts
+++ sanity/schemaTypes.ts
@@ -9,16 +9,38 @@ import bookingRequest from "./schemaTypes/bookingRequest";
 import contactRequest from "./schemaTypes/contactRequest"; // 👈 NIEUW
 import faqPage from "./schemaTypes/faqPage";
 
+// sanity/schemaTypes.ts
+import sitePage from "./schemaTypes/sitePage";
+import album from "./schemaTypes/album";
+import homePage from "./schemaTypes/homePage";
+import portfolioItem from "./schemaTypes/portfolioItem";
+import packageType from "./schemaTypes/package";
+import availabilitySettings from "./schemaTypes/availabilitySettings";
+import bookingRequest from "./schemaTypes/bookingRequest";
+
+// ✅ nieuw
+import contactPage from "./schemaTypes/contactPage";
+import contactRequest from "./schemaTypes/contactRequest";
+import faqPage from "./schemaTypes/faqPage";
+
 export const schemaTypes = [
-  sitePage,
-  album,
+  // Pagina’s / content
   homePage,
+  contactPage,
+  faqPage,
+  sitePage,
+
+  // Content
   portfolioItem,
+  album,
   packageType,
   availabilitySettings,
+
+  // Berichten
   bookingRequest,
   contactRequest,
-faqPage, // 👈 TOEGEVOEGD
 ];
+
+export default schemaTypes;
 
 export default schemaTypes;
